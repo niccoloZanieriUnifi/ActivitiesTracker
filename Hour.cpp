69a31a318e41f604311ad21ca53d01e2fdd0ac3d
niccolo <niccolo.zanieri.13@gmail.com>
2020-07-05 17:42:19 +0200

    Initial commit

---
+++ Hour.cpp
@@ -0,0 +1,5 @@
+//
+// Created by niccolo on 05/07/20.
+//
+
+#include "Hour.h"
---
+++ Hour.h
@@ -0,0 +1,14 @@
+//
+// Created by niccolo on 05/07/20.
+//
+
+#ifndef ACTIVITIESTRACKER_HOUR_H
+#define ACTIVITIESTRACKER_HOUR_H
+
+
+class Hour {
+
+};
+
+
+#endif //ACTIVITIESTRACKER_HOUR_H
